3853877f9218ea390cafe0df1be12c3518061f6c
심현진 <hyunjin.dev143@gmail.com>
2025-05-20 14:02:00 +0900

    chore: test logo 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import pdfqLogo from "/pdfq_logo.svg";
+import reactLogo from "./assets/react.svg";
 
 function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100 w-full">
-        <img src={pdfqLogo} alt="PDFQ Logo" />
+        <img src={reactLogo} alt="React Logo" />
         <p>this is test</p>
         <h1 className="text-3xl font-bold text-gray-700">PDF uploader</h1>
       </div>
